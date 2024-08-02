04e01a1cbadd914b601263059d67062972021497
Abdelrahman Mabrouk <Amabrouk@jeraisy.com>
2024-08-02 07:05:58 +0300

    deploy config

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   const routes = createBrowserRouter([
 
     {
-      path: 'foodApp',
+      path: '',
       element: <AuthLayout />,
       errorElement: <NotFound />,
       children: [
